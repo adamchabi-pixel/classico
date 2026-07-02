1ebc5212572385422357439a206760dfa7b78a39
adamchabi-pixel <adamchabi@hotmail.com>
2026-07-02 18:22:28 -0400

    chore: update favicon and manifest versions
    Refactor icon links to streamline the head section and bump the cache-busting version to 3.

--- update_head.py
+++ update_head.py
@@ -4,14 +4,10 @@ with open('index.html', 'r') as f:
     html = f.read()
 
 head_additions = """
-    <link rel="icon" type="image/x-icon" href="/favicon.ico?v=classico2" />
-    <link rel="icon" type="image/png" href="/favicon.png?v=classico2" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=classico2" />
-    <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png?v=classico2" />
-    <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png?v=classico2" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180.png?v=classico2" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=classico2" />
-    <link rel="manifest" href="/manifest.json?v=classico2" />
+    <link rel="icon" type="image/x-icon" href="/favicon.ico?v=3" />
+    <link rel="icon" type="image/png" href="/favicon.png?v=3" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=3" />
+    <link rel="manifest" href="/manifest.json?v=3" />
 """
 
 # Remove old icon links
@@ -20,8 +16,8 @@ html = re.sub(r'<link rel="apple-touch-icon"[^>]*>\n?', '', html)
 html = re.sub(r'<link rel="apple-touch-icon-precomposed"[^>]*>\n?', '', html)
 html = re.sub(r'<link rel="manifest"[^>]*>\n?', '', html)
 
-# Insert new ones before <meta name="viewport"
-html = html.replace('<meta name="viewport"', head_additions.strip() + '\n    <meta name="viewport"')
+# Clean up empty spaces caused by previous replacements
+html = re.sub(r'\s+<meta name="viewport"', '\n' + head_additions.rstrip() + '\n    <meta name="viewport"', html)
 
 with open('index.html', 'w') as f:
     f.write(html)
